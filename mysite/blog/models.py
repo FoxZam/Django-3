4981bda83986774d68fe2af77337c58b864be576
Artur Bajll <foxzampl@gmail.com>
2023-01-05 23:42:25 +0100

    Post PublishedManager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -18,6 +24,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish', )
